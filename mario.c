6574f986aa4d1182cb0db5abf4b55af260c46ba5
tw778 <tw778@users.noreply.github.com>
2018-03-03 18:52:06 +0000

    20180303T185152Z

--- mario.c
+++ mario.c
@@ -14,22 +14,22 @@ int main(void)
     {
         height = get_int("How high would you like your pyramid? Enter a positive integer between 0-23: ");
     }
-    while (height < 0 || height > 23);
+    while (height <= 0 || height > 23);
 
     //print a row
     for (rows = 0; rows < height; rows++)
     {
-        for (spaces = rows + 1; spaces < height; spaces++)
+        for (spaces = rows + 1; spaces <= 23; spaces++)
     {
-    printf(" ");
+            printf(" ");
     }
     //print hashes
         for (hashes = rows + 1; hashes >= 0; hashes--)
     {
-        printf("#");
+    printf("#");
     }
     //indent
-        printf("\n");
+    printf("\n");
     }
 }
 
